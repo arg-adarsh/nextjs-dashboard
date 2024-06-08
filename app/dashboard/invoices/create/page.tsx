eb7de9d04fa86725011c34851441db609cbf7302
Adarsh Raj Gautam <arg.adarsh@gmail.com>
2024-06-08 19:27:15 +0530

    Update

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,22 @@
+import { fetchCustomers } from '@/app/lib/data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '@/app/ui/invoices/create-form';
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoice', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: 'dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
